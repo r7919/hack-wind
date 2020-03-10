59f54b659b713bddb0312c15e28828f874859be7
Udaya Bhaskar <46437647+ubc1729@users.noreply.github.com>
2020-03-11 03:10:34 +0530

    Add files via upload

---
+++ sub_ids.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+import subprocess
+from bs4 import *
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+urls_file = open('out.txt', 'w')
+sys.stdout = urls_file
+
+# EDIT these
+contest_id = 1312
+problem_id = 557979
+page_start = 1
+page_end = 5
+
+x = "https://codeforces.com/contest/"+str(contest_id)+"/standings/page/"
+
+count_accepted = 0
+
+for k in range(page_start,page_end+1):
+    
+    y=str(str(x)+str(k))
+    response = requests.get(y)
+    html = response.content
+    soup = BeautifulSoup(html, "html5lib")
+    table = soup.find_all(problemid=problem_id)
+
+    for p in table:
+    	try:
+    		list_books = p.select(".acceptedsubmissionid")
+    		print("https://codeforces.com/contest/"+str(contest_id)+"/submission/"+str(p['acceptedsubmissionid']))
+    		count_accepted+=1
+    		eprint("scraping accepted submission URLS... ",count_accepted)
+    	except:
+    		pass
+
+# print(count_accepted)
+extracted = count_accepted
+urls_file.close()
